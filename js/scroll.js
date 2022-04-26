1271eb5df576b6ccbc490c24e127d6eed5b9dc9c
Darpan Chakraborty <darpanchakraborty2@gmail.com>
2022-04-26 17:19:59 +0530

    User profile image upload and additional input details add user appointment page create

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,7 @@
 // navbar color change
-$(window).scroll(function () {
+$(document).ready(function () {
+    $(window).scroll(function () {
     $('.nav-bar').toggleClass('scrolled', $(this).scrollTop() > 5);
     $('#arrow-nav').toggleClass('arrow-visible', $(this).scrollTop() > 500);
   });
+});
